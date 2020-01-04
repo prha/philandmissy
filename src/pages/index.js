d667bf923d9ce72f5b1b516a5605d6f1714bc6dc
Melissa Skevington <mskeving@gmail.com>
2020-01-03 16:53:21 -0800

    Update tweet feed to select and copy json of included tweets

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,7 @@ import { graphql } from "gatsby";
 export default ({ data }) => {
   return (
     <>
-      <div>Hello</div>
-      <div>
-        {data.allLoadyJson.edges.map((edge, i) => (
-          <div key={i}>
-            {edge.node.sender}: {edge.node.link}
-          </div>
-        ))}
-      </div>
-      <div>Goodbye</div>
+      <div>Hello how are you today.</div>
     </>
   );
 };
-
-export const query = graphql`
-  query {
-    allLoadyJson {
-      edges {
-        node {
-          sender
-          link
-        }
-      }
-    }
-  }
-`;
--- src/pages/tweets.js
+++ src/pages/tweets.js
@@ -2,34 +2,63 @@ import React from "react";
 import { graphql } from "gatsby";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
+const TWEET_ID_REGEX = /\d+/g
+
+const getTweetId = (link) => {
+  const parts = link.url.split("/");
+  const tweetPath = parts[parts.length - 1].split("?")[0];
+  const matches = tweetPath.match(TWEET_ID_REGEX)
+  return matches && matches.length && matches[0];
+}
+
 export default ({ data }) => {
+  const [includedLinks, setincludedLinks] = React.useState({});
+  const links = data.allDataJson.edges.map(x => x.node.twitter).find(x => x.length);
+  const toggleLink = (tweetId) => {
+    let newincludedLinks = {...includedLinks};
+    newincludedLinks[tweetId] = !includedLinks[tweetId];
+    setincludedLinks(newincludedLinks)
+  }
+  const displayLinks = links.filter(link => {
+    const tweetId = getTweetId(link);
+    return includedLinks[tweetId];
+  })
+
   return (
-    <div>
-      <form>
-        <div>
-          {data.allLinksJson.edges.map(({ node }) => {
-            const parts = node.url.split("/");
-            const tweetId = parts[parts.length - 1].split("?")[0];
-            return (
-              <div key={tweetId} className="centerContent">
-                <div className="selfCenter">
-                  <TwitterTweetEmbed tweetId={tweetId} />
-                </div>
+    <div style={{ display: 'flex', flexDirection: 'row'}}>
+      <div style={{flex: 1, borderRight: "1px solid #ececec"}}>
+        {links.map((link) => {
+          const tweetId = getTweetId(link);
+          const included = includedLinks[tweetId];
+          return (
+            <div key={tweetId} style={{ display: 'flex', flexDirection: 'row'}}>
+              <div>
+                <input type="checkbox" checked={included} onChange={() => toggleLink(tweetId)} style={{ margin: 20}} />
               </div>
-            );
-          })}
-        </div>
-      </form>
+              <div>
+              <TwitterTweetEmbed tweetId={tweetId} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div style={{flex: 1, padding: 10}}>
+        <textarea style={{width: '100%', height: '100%', border: 'none'}} value={JSON.stringify(displayLinks, null, 2)} readOnly/>
+      </div>
     </div>
   );
 };
 
 export const query = graphql`
   query {
-    allLinksJson {
+    allDataJson {
       edges {
         node {
-          url
+          twitter {
+            url
+            from
+            timestamp
+          }
         }
       }
     }
