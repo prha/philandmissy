53139f46cdd394d2de0c92b9d5ca86abd42b8d4e
prha <prha@elementl.com>
2020-01-16 17:59:20 -0800

    location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./index.scss";
 
+const locationUrl =
+  "https://www.google.com/maps/place/700+Crescent+Ave,+Saratoga+Springs,+NY+12866/@43.0561266,-73.7299642,14z";
+
 export default ({ data }) => {
   return (
     <div className="container">
-      <div className="card">
+      <div className="card column main">
         <div className="intro">An internet website to announce</div>
         <div className="title">
           <div className="part">Missy</div>
@@ -12,7 +15,40 @@ export default ({ data }) => {
           <div className="part">Phil's</div>
         </div>
         <div className="subtitle">Marital Union</div>
+        <Rsvp />
       </div>
+      <div className="card row location">
+        <div
+          className="subcard column text"
+          style={{ justifyContent: "space-between", alignItems: "flex-start" }}
+        >
+          <div className="address">
+            <div className="title">Saratoga Springs</div>
+            <div>700 Crescent Ave</div>
+            <div>Saratoga Springs, NY</div>
+            <div>12866</div>
+          </div>
+          <div className="directions">
+            <a href={locationUrl} target="_blank">
+              Get Directions &nbsp;&nbsp;&nbsp;&#8594;
+            </a>
+          </div>
+        </div>
+        <a
+          href={locationUrl}
+          className="subcard column map"
+          target="_blank"
+        ></a>
+      </div>
+      <div className="card"></div>
     </div>
   );
 };
+
+const Rsvp = () => (
+  <div className="rsvp">
+    <div className="frame1" />
+    <div className="frame2" />
+    RSVP
+  </div>
+);
